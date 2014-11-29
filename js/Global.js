a5dc23fece020ffc3c955e61f251b98d66f93f63
Isaac Cohen <isaaclandoncohen@gmail.com>
2014-11-29 11:56:16 -0500

    adding

--- js/Global.js
+++ js/Global.js
@@ -9,6 +9,21 @@ G.texturesToLoad = [
   ['matcap' ,'img/rough-aluminium.jpg' ]
   
 ]
+
+G.mobile = function () { 
+  if( navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+    return true;
+  } else {
+    return false;
+  }
+}();
 G.loader  = new Loader();
 
 
